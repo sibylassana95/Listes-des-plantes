273581e6e76ee67b75387742a01cac675608681e
sibylassana95 <sibyamara95@gmail.com>
2024-03-17 22:13:46 +0000

    update

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
+import requests
+from django.core.cache import cache
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import UserProfileForm
+
+
+def get_data():
+    url = 'https://raw.githubusercontent.com/sibylassana95/Listes-des-plantes/main/plante.json'
+    data = cache.get(url)
+    if not data:
+        response = requests.get(url)
+        data = response.json()
+        cache.set(url, data)
+    return data
+
+
+def index(request):
+    plantes = get_data()
+    query = request.GET.get('q')
+    if query:
+        plantes = [plante for plante in plantes if query.lower() in plante['name'].lower()]
+    context = {'plantes': plantes, 'query': query}
+    return render(request, 'index.html', context)
+
+
