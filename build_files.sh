5708ddcace351ea47c2c12712065efd7cbca8de0
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 15:03:59 +0000

    update

--- build_files.sh
+++ build_files.sh
@@ -11,6 +11,8 @@ pip install django
 pip install -r requirement.txt
 pip install django-cors-headers
 # collect static files using the Python interpreter from venv
+python3.9 manage.py migrate 
+python3.9 manage.py makemigrations
 python3.9 manage.py collectstatic 
 
 echo "BUILD END"
