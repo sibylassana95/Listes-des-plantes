18641657f297239d686148af384b4af6d3984548
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 14:24:53 +0000

    update

---
+++ build_files.sh
@@ -0,0 +1,18 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+pip install --upgrade setuptools
+# install all deps in the venv
+pip install -r requirements.txt
+pip install django-cors-headers
+# collect static files using the Python interpreter from venv
+python3.9 manage.py collectstatic 
+
+echo "BUILD END"
+
+# [optional] Start the application here 
+# python manage.py runserver
