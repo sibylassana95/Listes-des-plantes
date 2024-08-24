b5aa9804d2f61cc07b698d7b4ebe76700a39d669
sibylassana95 <sibyamara95@gmail.com>
2024-08-24 14:54:01 +0000

    update

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@ python3.9 -m venv venv
 source venv/bin/activate
 pip install --upgrade setuptools
 # install all deps in the venv
-pip install -r requirements.txt
+pip install -r requirement.txt
 pip install django-cors-headers
 # collect static files using the Python interpreter from venv
 python3.9 manage.py collectstatic 
